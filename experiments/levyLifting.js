e727d53e2c365cd80d03b4204a968b8a408c3e90
intertext.no <orstavik77@gmail.com>
2020-09-22 09:43:13 +0200

    levensthein cleaning

--- experiments/levyLifting.js
+++ experiments/levyLifting.js
@@ -33,13 +33,12 @@ function lowestTopLeftAction(res, i, j) {
 
 //todo make charOps iterate, not recursive
 function charOps(table, i, j, strX, strY) {
-let res = [];
-  while (i !== 0 && j !== 0) {
-    const [op, nextI, nextJ] = lowestTopLeftAction(table, i, j);
-    i = nextI;
-    j = nextJ;
-    res.unshift([op, nextJ, op === "D" ? strY[nextI] : strX[nextJ]]);
-  }
+  if (i === 0 && j === 0)
+    return [];
+  const [op, nextI, nextJ] = lowestTopLeftAction(table, i, j);
+  const res = charOps(table, nextI, nextJ, strX, strY);
+  res.push([op, nextJ, op === 'D' ? strY[nextI] : strX[nextJ]]);
+  return res;
 }
 
 export function levenshtein(a, b) {
