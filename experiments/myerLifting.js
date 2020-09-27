c16c7118ab8798700e75b67905915cdfc67bc7f8
intertext.no <orstavik77@gmail.com>
2020-09-27 09:54:32 +0200

    string coordinate

--- experiments/myerLifting.js
+++ experiments/myerLifting.js
@@ -8,11 +8,19 @@ function makeBranch(x, y, ref, tar) {
   let n = 0;
   while (ref[x + n] === tar[y + n] && x + n < ref.length && y + n < tar.length)
     n++;
-  const end = (x + n) + ',' + (y + n);
+  const end = toCoordinate(x + n, y + n);
   if (pointsVisited.indexOf(end) !== -1) //set up a single 2d array to keep both the points and the points visited as one.
     return null;
   pointsVisited.push(end);
-  return [[x + n, y + n]];
+  return [end];
+}
+
+function toCoordinate(x, y) {
+  return x + ',' + y;
+}
+
+function fromCoordinate(p) {
+  return p.split(',').map(s => parseInt(s));
 }
 
 export function myersDiff(tar, ref) {
@@ -23,17 +31,19 @@ export function myersDiff(tar, ref) {
   while (ref[n] === tar[n])
     n++;
 
-  let branches = [[[n, n]]];
+  const theVeryEnd = toCoordinate(ref.length, tar.length);
+
+  let branches = [[n + ',' + n]];
   while (true) {
     let nextBranches = [];
     for (let i = 0; i < branches.length; i++) {
       let branch = branches[i];
-      let [x, y] = branch[0];
+      let [x, y] = fromCoordinate(branch[0]);
       //go right/delete from a
       if (x < ref.length) {
         let res = makeBranch(x + 1, y, ref, tar);
         if (res) {
-          if (res[0][0] === ref.length && res[0][1] === tar.length)
+          if (res[0] === theVeryEnd)
             return postProcess(res.concat(branch), ref, tar);
           nextBranches.push(res.concat(branch));
         }
@@ -42,7 +52,7 @@ export function myersDiff(tar, ref) {
       if (y < tar.length) {
         let res = makeBranch(x, y + 1, ref, tar);
         if (res) {
-          if (res[0][0] === ref.length && res[0][1] === tar.length)
+          if (res[0] === theVeryEnd)
             return postProcess(res.concat(branch), ref, tar);
           nextBranches.push(res.concat(branch));
         }
@@ -57,9 +67,7 @@ function postProcess(res, ref, tar) {
   let oneX = 0;
   let oneY = 0;
   for (let i = res.length - 2; i >= 0; i--) {
-    let two = res[i];
-    const nextX = two[0];
-    const nextY = two[1];
+    const [nextX, nextY] = fromCoordinate(res[i]);
     const distX = nextX - oneX;
     const distY = nextY - oneY;
     const min = Math.min(distX, distY);
