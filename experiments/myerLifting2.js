46c3136d93d2659f2deeb19b390ba85d4ef3b827
intertext.no <orstavik77@gmail.com>
2020-09-28 21:49:47 +0200

    babystep improvements

--- experiments/myerLifting2.js
+++ experiments/myerLifting2.js
@@ -10,23 +10,23 @@ export function myersDiff(tar, ref) {
   while (tar[n] === ref[n])
     n++;
 
-  let res = [{0: n}, {0: n}];
+  const res = [{0: n}];
   for (let d = 1; d < 10000; d++) {
-    res[d + 1] = {};
+    res[d] = {};
+    //todo not sure about this capping..Not sure it is ever useful, and not sure that it is safe. If one of the texts is a lot shorter than the other, it might make sense though..
+    //bug, doesn't work
+    // if (k < -tar.length || k > ref.length)
+    //   continue;
     for (let k = -d; k <= d; k += 2) {
-      //todo not sure about this capping..Not sure it is ever useful, and not sure that it is safe. If one of the texts is a lot shorter than the other, it might make sense though..
-      //bug, doesn't work
-      // if (k < -tar.length || k > ref.length)
-      //   continue;
-      const previousUpIsBest = k === -d || k !== d && res[d - 1][k + 1] > res[d - 1][k - 1];  //true if we are coming down, false if we are coming up.
-      const previousK = previousUpIsBest ? k + 1 : k - 1;
+      const comingDown = k === -d || k !== d && res[d - 1][k + 1] > res[d - 1][k - 1];
+      const previousK = comingDown ? k + 1 : k - 1;
       const previousX = res[d - 1][previousK];
-      const nowX = previousX + (previousUpIsBest ? 0 : 1);
+      const nowX = comingDown ? previousX : previousX + 1;
       const nowY = nowX - k;
       let n = 0;
       while (ref[nowX + n] === tar[nowY + n] && nowY + n < endX && nowY + n < endY)
         n++;
-      res[d][k] = res[d + 1][k] = nowX + n;
+      res[d][k] = nowX + n;
       if (nowX + n === endX && nowY + n === endY)
         return postProcess(mapToXY(res, d, k), ref, tar);
     }
@@ -39,7 +39,7 @@ function mapToXY(res, d, k) {
   for (let i = d; i > 0; i--) {
     const x = res[i][k];
     coords[i] = [x, x - k];
-    res[i][k - 1] > res[i][k + 1] || res[i][k + 1] === undefined ? k-- : k++;
+    res[i-1][k - 1] > res[i-1][k + 1] || res[i-1][k + 1] === undefined ? k-- : k++;
   }
   return coords;
 }
